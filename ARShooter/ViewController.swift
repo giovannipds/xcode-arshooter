3bbeb1b81c8e5d7d65d6609d17f74d376e5e4444
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-02 03:17:35 -0300

    Set up the sceneView and add egg targets

--- ARShooter/ViewController.swift
+++ ARShooter/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addTargets(_ sender: Any) {
+        self.addEgg(x: 5, y: 0, z: -40)
+        self.addEgg(x: 0, y: 0, z: -40)
+        self.addEgg(x: -5, y: 0, z: -40)
+        
+    }
+    
+    func addEgg(x: Float, y: Float, z: Float) {
+        let eggScene = SCNScene(named: "Media.scnassets/egg.scn")
+        let eggNode = (eggScene?.rootNode.childNode(withName: "egg", recursively: false))!
+        eggNode.position = SCNVector3(x,y,z)
+        self.sceneView.scene.rootNode.addChildNode(eggNode)
+    }
+    
 }
 
