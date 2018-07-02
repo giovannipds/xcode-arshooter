08a94c7b5a16639cd54aed5082ee003ff4bd3360
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-02 04:16:20 -0300

    Add the smoke animation

--- ARShooter/ViewController.swift
+++ ARShooter/ViewController.swift
@@ -83,7 +83,7 @@ class ViewController: UIViewController, SCNPhysicsContactDelegate {
         } else if nodeB.physicsBody?.categoryBitMask == BitMaskCategory.bullet.rawValue {
             self.Target = nodeB
         }
-        let confetti = SCNParticleSystem(named: "Media.scnassets/Reactor.scnp", inDirectory: nil)
+        let confetti = SCNParticleSystem(named: "Media.scnassets/Smoke.scnp", inDirectory: nil)
         confetti?.loops = false
         confetti?.particleLifeSpan = 4
         confetti?.emitterShape = Target?.geometry
